761da3144810e1c0fd00ad57d71504dacca49c8d
Celcus <arandomguy1337@gmail.com>
2017-07-13 17:22:42 -0500

    Create rng.java

---
+++ KotA/src/rng.java
@@ -0,0 +1,26 @@
+package rng;
+
+public class KotA_Decision{
+    
+    public static String coinFlip(){ //to decide which team will pick a server
+        int gen = (int)(Math.random()*2+1);
+        if(gen == 1){
+            return "Heads";
+        }else if(gen == 2){
+            return "Tails";
+        }else{
+            return "";
+        }
+    }
+    
+    public static String MapDecision(){ //if both teams can't decide a map
+        String[] maps = {"Canals", "Bureau", "Legacy", "Plaza", "Grounded"};
+        int map = (int)(Math.random()*maps.length);
+        return maps[map];
+    }
+    
+    public static void main(String[] args){
+        System.out.print("coinflip side: "+ coinFlip()+
+            "\nMap decided:: " + MapDecision()+"\n");
+    }
+}
